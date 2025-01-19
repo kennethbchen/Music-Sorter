540e6b0e30dc32558ef5081467c2fd423a8fd50d
kennethbchen <kennethbchen@gmail.com>
2025-01-19 14:59:26 -0500

    Implemented getting input / output paths from config file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 use config::Config;
 
 fn main() {
 
-    // Parse Config
+    // Parse config file
     let config = Config::builder()
         .add_source(config::File::with_name("config.toml"))
         .build();
@@ -16,5 +16,15 @@ fn main() {
 
     let config: Config = config.unwrap();
 
+    // Get settings from config file
+    let input_path = &config.get_string("input_folder").unwrap();
+    let input_path: &Path = Path::new(input_path);
+
+    let output_path = &config.get_string("output_folder").unwrap();
+    let output_path: &Path = Path::new(output_path);
+
+    println!("{:?}", input_path);
+    println!("{:?}", output_path);
+    
 }
 
