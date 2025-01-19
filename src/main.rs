b50a2eb105b10740f55372e9eee3bc25efef7f38
kennethbchen <kennethbchen@gmail.com>
2025-01-19 14:43:41 -0500

    Started implementing config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+
+use config::Config;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Parse Config
+    let config = Config::builder()
+        .add_source(config::File::with_name("config.toml"))
+        .build();
+
+    match config {
+        Ok(_) => (),
+        Err(_) => panic!("config.toml not found")
+    }
+
+    let config: Config = config.unwrap();
+
 }
+
