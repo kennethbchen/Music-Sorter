0b69f0cdc666a05c412119c211f6fe61172699b1
kennethbchen <kennethbchen@gmail.com>
2025-01-22 09:45:56 -0500

    Started implementing getting metadata

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::{collections::HashMap, fs, path::{Path, PathBuf}, str::FromStr};
+use std::io::Error;
+
+use std::{fs, path::PathBuf, str::FromStr};
+
+use std::fs::File;
 
 use config::Config;
+
+use zip::ZipArchive;
+
+use id3::{Tag, TagLike};
 
 struct AppSettings {
     input_path: PathBuf,
@@ -8,9 +16,9 @@ struct AppSettings {
 }
 
 enum SupportedInput {
-    Dir(PathBuf),
-    Zip(PathBuf),
-    Mp3(PathBuf)
+    DirInput(PathBuf),
+    ZipInput(PathBuf),
+    Mp3Input(PathBuf)
 }
 
 
@@ -18,7 +26,7 @@ impl SupportedInput {
     fn from(path: PathBuf) -> Option<SupportedInput> {
 
         if path.is_dir() {
-            return Option::Some(SupportedInput::Dir(path));
+            return Option::Some(SupportedInput::DirInput(path));
         }
 
         match path.extension() {
@@ -26,15 +34,16 @@ impl SupportedInput {
 
                 match extension_str.to_str().unwrap() {
                     "zip" => {
-                        return Option::Some(SupportedInput::Zip(path));
+                        return Option::Some(SupportedInput::ZipInput(path));
                     },
                     "mp3" => {
-                        return Option::Some(SupportedInput::Mp3(path));
+                        return Option::Some(SupportedInput::Mp3Input(path));
                     },
                     _ => return None
 
                 }
             }
+
             None => return None
         }
     }
@@ -72,11 +81,66 @@ fn parse_config() -> AppSettings {
     }
 }
 
-fn get_destination_path(output_path: &PathBuf, input: SupportedInput) {
-    match input {
-        SupportedInput::Dir(path) => println!("Dir"),
-        SupportedInput::Zip(path) => println!("Zip"),
-        SupportedInput::Mp3(path) => println!("Mp3")
+fn get_destination_path(output_folder_path: &PathBuf, file: SupportedInput) -> Result<PathBuf, Error> {
+    match file {
+        SupportedInput::DirInput(path) => todo!(),
+        SupportedInput::ZipInput(path) => {
+            
+
+            let zip = File::open(path)?;
+
+            let mut zip = ZipArchive::new(zip)?;
+            
+            // Find an Mp3 file to determine destination path
+            for i in 0..zip.len() {
+                
+                
+                
+                // Check if this file is an mp3
+                {
+                    let file = zip.by_index(i)?;
+
+                    let filepath = file.enclosed_name();
+
+                    match &filepath {
+                        Option::Some(_) => (),
+                        Option::None => {continue;}
+                    }
+
+                    let filepath = filepath.unwrap();
+
+                    let file = SupportedInput::from(filepath);
+
+                    if file.is_none() {
+                        continue;
+                    }
+
+                    let file = file.unwrap();
+
+                    match file {
+                        SupportedInput::Mp3Input(_) => (),
+                        _ => continue
+                    }
+                }
+                
+                // Get song metadata
+                let file = &mut zip.by_index_seek(i)?;
+                
+                let tag = Tag::read_from2(file).unwrap();
+
+                println!("{:?} {:?}", tag.artist(), tag.album_artist());
+                
+                
+                
+                
+                
+                
+                //print!("{:?}", filepath.extension());
+            }
+            
+            return Result::Ok(PathBuf::new());
+        },
+        SupportedInput::Mp3Input(path) => todo!()
     }
 }
 
@@ -91,7 +155,19 @@ fn main() {
 
         match entry {
             Some(input) => {
-                get_destination_path(&settings.output_path, input);
+
+                let path = get_destination_path(&settings.output_path, input);
+
+                match path {
+                    Result::Ok(_) => (),
+                    Result::Err(_) => {
+                        println!("Error getting path, skipping");
+                        continue;
+                    }
+                }
+
+                let path = path.unwrap();
+
             },
             None => println!("Skipping unsupported input...")
         }
